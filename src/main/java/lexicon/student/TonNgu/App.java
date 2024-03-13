2ae93a7d7cb2129acf82b029f89e4b6692643d73
Tony Nguyen <tonynguyen7396@gmail.com>
2024-03-13 09:28:17 +0100

    Added names for methods

--- src/main/java/lexicon/student/TonNgu/App.java
+++ src/main/java/lexicon/student/TonNgu/App.java
@@ -10,4 +10,21 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+
+
+
 }
+
+public class NameRepository
+{
+    private static String[] names
+}
+
+public static int getSize(){}
+
+public static void setNames (String[] names)
+
+public static void clear()
+
+public static String[] findAll()
+
