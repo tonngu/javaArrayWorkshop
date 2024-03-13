aaa391fea18569b2b0ce3ada6730e59a5e89d124
Tony Nguyen <tonynguyen7396@gmail.com>
2024-03-13 17:57:50 +0100

    definied method remove

--- src/main/java/lexicon/student/TonNgu/App.java
+++ src/main/java/lexicon/student/TonNgu/App.java
@@ -7,7 +7,14 @@ public class App {
         String[] newNames = {"Test Name1", "Test Name2", "Test Name3", "Test Name4", "Test Name5"};
         NameRepository.setNames(newNames); //store names from newNames array
 
+        NameRepository.remove("Test Name4");
+
+        for (String abc : NameRepository.findAll()) { //print all stored names
+            System.out.println(abc);
+        }
+
         /*
+
         String findTestResult = NameRepository.find("Test Name5"); //check if this name is already stored
         if (findTestResult != null) {
             System.out.println("Name found");
@@ -22,18 +29,15 @@ public class App {
             System.out.println("New name added");
         }
 
-            for (String abc : NameRepository.findAll()) { //print all stored names
-            System.out.println(abc);
-        }
-
         for (String abcd : NameRepository.findByFirstName("Test")) { //look for names containing this first name and put them into an array and then prints each element out
             System.out.println(abcd);
         }
-        */
 
         for (String abcde : NameRepository.findByLastName("Name4")) { //same as above but for last names
             System.out.println(abcde);
         }
+
+        */
     }
 
 }
--- src/main/java/lexicon/student/TonNgu/NameRepository.java
+++ src/main/java/lexicon/student/TonNgu/NameRepository.java
@@ -17,15 +17,11 @@ public class NameRepository {
         names = new String[0];
     }
 
-    public static String[] findAll() { //return the content of current names array in a new array newNames
-        String[] findAllResult = new String[names.length];
-        for (int i = 0; i < names.length; i++) {
-            findAllResult[i] = names[i];
-        }
-        return findAllResult;
+    public static String[] findAll() { //return the content of current names array in a new array findAllResult
+        return Arrays.copyOf(names, names.length);
     }
 
-    public static String find(final String fullName) {
+    public static String find(final String fullName) { //Find if a name exists within the currently stored names, returns null if not
         for (String check : names) {
             if (check.equals(fullName)) {
                 return check;
@@ -34,16 +30,54 @@ public class NameRepository {
         return null;
     }
 
-    public static boolean add(final String fullName) {
-
+    public static boolean add(final String fullName) { //Add a name and returns true if the name doesn't yet exist within currently stored names, else returns false
         if (find(fullName) == null) {
-            String addedName = fullName;
             String[] copiedArray = Arrays.copyOf(names, names.length + 1);
-            copiedArray[copiedArray.length - 1] = addedName;
+            copiedArray[copiedArray.length - 1] = fullName;
             names = copiedArray;
             return true;
         } else {
             return false;
         }
     }
+
+    public static String[] findByFirstName(final String firstName) { //search through stored names by firstName, returns an array with all hit
+        String[] searchResult = new String[0];
+        for (String name : names) {
+            if (name.startsWith(firstName)) {
+                searchResult = Arrays.copyOf(searchResult, searchResult.length + 1);
+                searchResult[searchResult.length - 1] = name;
+            }
+        }
+        return searchResult;
+    }
+
+    public static String[] findByLastName(final String lastName) { //search through stored names by lastName, returns an array with all hit
+        String[] searchResult = new String[0];
+        for (String name : names) {
+            if (name.endsWith(lastName)) {
+                searchResult = Arrays.copyOf(searchResult, searchResult.length + 1);
+                searchResult[searchResult.length - 1] = name;
+            }
+        }
+        return searchResult;
+    }
+
+    public static boolean remove(final String fullName) { //Remove a specified name from the currently stored names and return a boolean statement whether or not the name was removed
+        if (find(fullName) != null) {
+            String[] searchResult = new String[0];
+
+            for (String name : names) {
+                if (!name.equals(fullName)) {
+                    searchResult = Arrays.copyOf(searchResult, searchResult.length + 1);
+                    searchResult[searchResult.length - 1] = name;
+                }
+            }
+            names = searchResult;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
