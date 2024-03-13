bc53967c5926cdbdb1074feb01a9dae2be700e5e
Tony Nguyen <tonynguyen7396@gmail.com>
2024-03-13 16:43:29 +0100

    defined methods findByFirstName and findByLastName

--- src/main/java/lexicon/student/TonNgu/App.java
+++ src/main/java/lexicon/student/TonNgu/App.java
@@ -1,12 +1,14 @@
 package lexicon.student.TonNgu;
 
+import javax.naming.Name;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
 
         //testing NameRepository functions
-      String[] newNames = {"Test Name1","Test Name2", "Test Name3", "Test Name4", "Test Name5" };
+        String[] newNames = {"Test Name1", "Test Name2", "Test Name3", "Test Name4", "Test Name5"};
         NameRepository.setNames(newNames); //store names from newNames array
 
         /*String findTestResult = NameRepository.find("Test Name5"); //check if the name here is already stored
@@ -14,20 +16,26 @@ public class App {
             System.out.println("Name found");
         } else {
             System.out.println("Name not found");
-        }*/
+        }
 
-        if (NameRepository.add("Test Name6") == false){
+        if (NameRepository.add("Test Name6") == false){ //Look for the specified name in the NameRepository, if not found then it will be added.
         System.out.println("Name already added");
         }
         else{
             System.out.println("New name added");
         }
 
-        String[] allStoredNames = NameRepository.findAll();
-        for (String abc : allStoredNames) { //print all stored names
+            for (String abc : NameRepository.findAll()) { //print all stored names
             System.out.println(abc);
         }
 
+        for (String abcd : NameRepository.findByFirstName("Test")) { //look for names containing this first name and put them into an array and then prints each element out
+            System.out.println(abcd);
+        }*/
+
+        for (String abcde : NameRepository.findByLastName("Name4")) { //same as above but for last names
+            System.out.println(abcde);
+        }
     }
 
 }
