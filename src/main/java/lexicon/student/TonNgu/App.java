7d92a8b59473ac5facbd187c38fc7d9c4a3be470
Tony Nguyen <tonynguyen7396@gmail.com>
2024-03-13 16:57:50 +0100

    changed findAll from using for loop to copyOf method instead

--- src/main/java/lexicon/student/TonNgu/App.java
+++ src/main/java/lexicon/student/TonNgu/App.java
@@ -1,9 +1,5 @@
 package lexicon.student.TonNgu;
 
-import javax.naming.Name;
-import java.sql.SQLOutput;
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) {
 
@@ -11,14 +7,15 @@ public class App {
         String[] newNames = {"Test Name1", "Test Name2", "Test Name3", "Test Name4", "Test Name5"};
         NameRepository.setNames(newNames); //store names from newNames array
 
-        /*String findTestResult = NameRepository.find("Test Name5"); //check if the name here is already stored
+        /*
+        String findTestResult = NameRepository.find("Test Name5"); //check if this name is already stored
         if (findTestResult != null) {
             System.out.println("Name found");
         } else {
             System.out.println("Name not found");
         }
 
-        if (NameRepository.add("Test Name6") == false){ //Look for the specified name in the NameRepository, if not found then it will be added.
+        if (NameRepository.add("Test Name6") == false){ //Look for the specified name in the NameRepository, if not found then it will be added
         System.out.println("Name already added");
         }
         else{
@@ -31,7 +28,8 @@ public class App {
 
         for (String abcd : NameRepository.findByFirstName("Test")) { //look for names containing this first name and put them into an array and then prints each element out
             System.out.println(abcd);
-        }*/
+        }
+        */
 
         for (String abcde : NameRepository.findByLastName("Name4")) { //same as above but for last names
             System.out.println(abcde);
