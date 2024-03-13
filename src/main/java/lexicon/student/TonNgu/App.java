9997a628c94d9dd05fa4154fcea63130aaf1376c
Tony Nguyen <tonynguyen7396@gmail.com>
2024-03-13 09:53:11 +0100

    definied getSize

--- src/main/java/lexicon/student/TonNgu/App.java
+++ src/main/java/lexicon/student/TonNgu/App.java
@@ -2,29 +2,38 @@ package lexicon.student.TonNgu;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Not Hello World!");
+    }
+
+}
+
+public class NameRepository {
+    private static String[] names = new String[3] {
+
+            public static int getSize () {
+            return names.length;
+
     }
 
 
+    }
+
+    public static void setNames(String[] names) {
+
+    }
+
+    public static void clear() {
+
+    }
+
+    public static String[] findAll() {
+
+    }
+
 
 }
 
-public class NameRepository
-{
-    private static String[] names
-}
-
-public static int getSize(){}
-
-public static void setNames (String[] names)
-
-public static void clear()
-
-public static String[] findAll()
 
